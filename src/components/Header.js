1beaba803400ead5f553ac3a456b3a480c2a148a
Jules Scheil <juliana.m.scheil@gmail.com>
2021-05-15 13:49:47 -0400

    added components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Employee Directory</h1>
+        <p>Click to filter or you can search.</p>
+      </div>
+    )
+  }
+}
