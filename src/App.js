16f6d987e8a644251889d1b61499528c75ccc18e
Jules Scheil <juliana.m.scheil@gmail.com>
2021-05-24 19:49:30 -0400

    added search results section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-// import Main from "./components/Main";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import Home from './Pages/home';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header />
       <Wrapper>
         <Home />
       </Wrapper>
     </div>
+    </Router>
   );
 }
 
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,1 +1,35 @@
 import React from 'react';
+
+function Results(props) {
+    return (
+        <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope='col'>Name</th>
+                                <th scope='col'>Phone</th>
+                                <th scope='col'>Email</th>
+                                <th scope='col'>DOB</th>
+                            </tr>
+                        </thead>
+                        <tbody className="table">
+                            {props.results.map(employee => {
+                                <tr className="row">
+                                    <td>{employee.name.first} {employee.name.last}</td>
+                                    <td>{employee.phone}</td>
+                                    <td>{employee.email}</td>
+                                    <td>{employee.dob.date}</td>
+                                </tr>
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Results;
